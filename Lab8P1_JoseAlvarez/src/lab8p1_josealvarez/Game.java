7b13b4bf96d6c8cbfddace89335a8ba35b7f231e
josefalvarezb <147879331+josefalvarezb@users.noreply.github.com>
2023-12-01 15:18:09 -0600

    Add files via upload

---
+++ Lab8P1_JoseAlvarez/src/lab8p1_josealvarez/Game.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8p1_josealvarez;
+
+/**
+ *
+ * @author jkalvarezb
+ */
+public class Game {
+    public int [][] generacionact;
+    public int [][] generaciondesp;
+    public int rondasPorJugar;
+    
+    public Game(){
+        this.generacionact = new int[10][10];
+        this.generaciondesp = new int [10][10];
+        this.rondasPorJugar = 0;
+    }
+    
+    
+    
+    
+
+}
---
+++ Lab8P1_JoseAlvarez/src/lab8p1_josealvarez/Lab8P1_JoseAlvarez.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+//Fila 2 asiento 10 JoseAlvarez
+
+package lab8p1_josealvarez;
+
+/**
+ *
+ * @author jkalvarezb
+ */import java.util.Scanner;
+
+public class Lab8P1_JoseAlvarez {
+
+
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        Game game = new Game();
+        
+        System.out.println("Ingrese el numero de rondas: ");
+        int rounds = leer.nextInt();
+        
+        
+        
+    }
+    
+}
